015cb3ed69aa98c67dbf36068e2a66f063bc53c6
Aleksandar Ivanovski <aleksandar.ivanovski123@gmail.com>
2020-11-19 16:47:54 +0100

    Fix Lab 03

--- Lab03/task_2.cpp
+++ Lab03/task_2.cpp
@@ -6,7 +6,8 @@
 #define BUFFER_SIZE 100
 #define REQUEST_SIZE 20
 
-int main() {
+int main()
+{
     MPI_Init(NULL, NULL);
 
     int world_size;
@@ -24,26 +25,27 @@ int main() {
 
     int j = 0;
 
-    if (rank == 0) {
+    if (rank == 0)
+    {
         strcpy(send, "Message from CPU0 to all CPUs");
-        for (int proc_i = 1; proc_i < world_size; ++proc_i) {
+        for (int proc_i = 1; proc_i < world_size; ++proc_i)
+        {
             MPI_Isend(send, strlen(send) + 1, MPI_BYTE, proc_i, 0, MPI_COMM_WORLD, &reqs[proc_i]);
         }
         MPI_Irecv(recv, BUFFER_SIZE, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD, &reqs[0]);
+
         j = world_size;
-    } else {
+    }
+    else
+    {
         MPI_Irecv(recv, BUFFER_SIZE, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &reqs[0]);
+        printf("[%d] : %s\n", rank, recv);
+        sprintf(send, "Answer from #%d to CPU0", rank);
+        MPI_Isend(send, strlen(send) + 1, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &reqs[j]);
         j++;
-        if (rank == world_size - 1) {
-            sprintf(send, "Answer from #%d to CPU0", rank);
-            MPI_Isend(send, strlen(send) + 1, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &reqs[1]);
-            j++;
-        }
     }
 
     MPI_Waitall(j, reqs, statuses);
-
-    printf("[%d] : %s\n", rank, recv);
 
     MPI_Finalize();
     return 0;
