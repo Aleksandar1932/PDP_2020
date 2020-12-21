f713c40bd709382404ed1806e1f83e4ffd4a6329
Aleksandar1932 <aleksandar.ivanovski123@gmail.com>
2020-12-21 22:10:11 +0100

    Complete Lab 05 in testing env
    Bug fixes
    Refactor
    Ready for deployment on hadoop cluster

--- Lab05/LogAnalysis/src/main/java/LogAnalysis.java
+++ Lab05/LogAnalysis/src/main/java/LogAnalysis.java
@@ -1,21 +1,32 @@
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class LogAnalysis {
     public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
-        public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+        private String getMonthAndFileKeyFromLogLine(String line) {
+            String month = line.split("/")[1];
+            String file = Paths.get(line.split("GET")[1].split("HTTP")[0]).getFileName().toString();
+
+            return String.format("%s %s", month, file);
+        }
+
+        private String getIpFromLogLine(String line) {
+            return line.split("\\s")[0];
+        }
+
+        public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) {
             List<String> lines = Arrays.asList(value.toString().split("\n"));
             lines.forEach(line -> {
-                String month = line.split("/")[1];
-                String ip = line.split("\\s")[0];
+                String monthAndFileKey = getMonthAndFileKeyFromLogLine(line);
+                String ip = getIpFromLogLine(line);
                 try {
-                    output.collect(new Text(month), new Text(ip));
+                    output.collect(new Text(monthAndFileKey), new Text(ip));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -23,14 +34,10 @@ public class LogAnalysis {
         }
     }
 
-    public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Set<Text>> {
-        public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Set<Text>> output, Reporter reporter) throws IOException {
-            Set<Text> addresses = new HashSet<>();
-
-            while(values.hasNext()){
-                addresses.add(values.next());
-            }
-
+    public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Set<String>> {
+        public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Set<String>> output, Reporter reporter) throws IOException {
+            Set<String> addresses = new HashSet<>();
+            values.forEachRemaining(value -> addresses.add(value.toString()));
             output.collect(key, addresses);
         }
     }
